5ca8f4bfdc41b9c6d63795b9344c352f88b1d9cf
Pablo <pablojose.mingo@tajamar365.com>
2026-05-24 18:15:24 +0200

    fix: restaurado mensaje de prueba estático en el backend

--- api/GetDatabaseData/index.js
+++ api/GetDatabaseData/index.js
@@ -1,7 +1,12 @@
 module.exports = async function (context, req) {
     context.res = {
         status: 200,
-        headers: { "Content-Type": "application/json" },
-        body: { mensaje: "Hola desde el backend Serverless de Azure", timestamp: new Date() }
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: {
+            mensaje: "Hola desde el backend Serverless de Azure",
+            timestamp: new Date().toISOString()
+        }
     };
 };
